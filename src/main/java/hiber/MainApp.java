8da1e9d3ee05c3fc76a6eda818f7abe7574b2838
Evgeny Anchutin <exs@bk.ru>
2021-03-22 23:29:55 +0300

    Initial commit

---
+++ src/main/java/hiber/MainApp.java
@@ -0,0 +1,46 @@
+package hiber;
+
+import hiber.config.AppConfig;
+import hiber.model.Car;
+import hiber.model.User;
+import hiber.service.UserService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class MainApp {
+   public static void main(String[] args) throws SQLException {
+      AnnotationConfigApplicationContext context = 
+            new AnnotationConfigApplicationContext(AppConfig.class);
+
+      UserService userService = context.getBean(UserService.class);
+
+      Car lada = new Car("Lada-sedan", 1);
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru", lada));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru", lada));
+      Car ford = new Car("Ford-escort", 1);
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru", ford));
+      Car fiat = new Car("Fiat-500", 500);
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru", fiat));
+
+      List<User> users = userService.listUsers();
+      printUsers(users);
+
+      users = userService.listUsersByCar("Lada-sedan", 1);
+      printUsers(users);
+
+      context.close();
+   }
+
+   private static void printUsers(List<User> users) {
+      for (User user : users) {
+         System.out.println("Id = "+user.getId());
+         System.out.println("First Name = "+user.getFirstName());
+         System.out.println("Last Name = "+user.getLastName());
+         System.out.println("Email = "+user.getEmail());
+         System.out.println("Car = " + user.getCar());
+         System.out.println();
+      }
+   }
+}
---
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -0,0 +1,42 @@
+package hiber.dao;
+
+import hiber.model.User;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Repository
+public class UserDaoImp implements UserDao {
+
+   @Autowired
+   private SessionFactory sessionFactory;
+
+   @Override
+   public void add(User user) {
+      sessionFactory.getCurrentSession().save(user);
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsers() {
+      TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("FROM User");
+      return query.getResultList();
+   }
+
+   @Override
+   @SuppressWarnings("unchecked")
+   public List<User> listUsersByCar(String model, int series) {
+      String hql = "SELECT user FROM User user " +
+              "JOIN user.car car " +
+              "JOIN FETCH user.car " +
+              "WHERE car.model = :model " +
+              "AND car.series = :series";
+      TypedQuery<User> query = sessionFactory.getCurrentSession().createQuery(hql);
+      query.setParameter("model", model);
+      query.setParameter("series", series);
+      return query.getResultList();
+   }
+
+}
